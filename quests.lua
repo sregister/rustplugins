1a197e18dbb9fcef2c218f458b67c2eff5864440
Scott Register <scott.register@gmail.com>
2014-01-18 12:59:08 -0800

    updated so i think that quests will work with version 1.8

--- quests.lua
+++ quests.lua
@@ -1,20 +1,19 @@
---note this is not my plugin, all credit to Greyhawk's Quests Plugin 
---more info at: http://forum.rustoxide.com/resources/quests.31/
+--updated, hopefully 1.8 works :/
 
 PLUGIN.Title = "Quests"
 PLUGIN.Description = "Basic Quest Plugin"
 
 function PLUGIN:Init()
-    econ_mod = cs.findplugin("econ")
+    econ_mod = plugins.Find("econ")
+    
     if (not econ_mod) then
         self.econ = false
     else
         self.econ = true
     end
     
-    self.autojoin = true
-
-	-- self.DataFile = datafile( "quests" )
+    self.autojoin = true 
+	-- self.DataFile = util.GetDatafile( "quests" )
 	-- local txt = self.DataFile:GetText()
 	-- if (txt ~= "") then
 		-- self.Quests = json.decode( txt )
@@ -23,19 +22,20 @@ function PLUGIN:Init()
 	-- end
     
     self.Zombies = {}
-    self:addZombieQuest("Zombies", "5", "Can of Tuna", "1")
-    self:addZombieQuest("MoreZombies", "25", "Research Kit 1", "1")
-    self:addZombieQuest("So Many Zombies", "250", "Supply Signal", "1")
+    self:addZombieQuest("Zombies", "2", "Can of Tuna", "1")
+    self:addZombieQuest("MoreZombies", "20", "M4", "1")
+    -- self:addZombieQuest("So Many Zombies", "200", "Supply Signal", "1")
     if (self.econ) then
         local questname = "ZombiesMoney"
-        local killcount = "2"
-        local rewardcount = "20"
+        local killcount = "10"
+        local rewardcount = "50"
         table.insert(self.Zombies, questname)
         self:addQuest( questname,
                     "kill " .. killcount .. " zombies to get ".. rewardcount .. econ_mod.CurrencySymbol,
                     (function(x) return (x >= tonumber(killcount)) end),
                     (function(user) econ_mod:giveMoneyTo(user, tonumber(rewardcount)) end)
                     )
+        
     end
 
 	self:AddChatCommand("quest", self.cmdQuest)
@@ -47,7 +47,7 @@ function PLUGIN:addZombieQuest(questname, killcount, questreward, rewardcount)
     self:addQuest( questname,
                     "kill " .. killcount .. " zombies to get ".. rewardcount .. " " .. questreward,
                     (function(x) return (x >= tonumber(killcount)) end),
-                    (function(user) rust.RunServerCommand("inv.giveplayer \"" .. rust.QuoteSafe( user.displayName ) .. "\" \"" .. questreward .. "\" " .. rewardcount ) end)
+                    (function(user) rust.RunServerCommand("inv.giveplayer \"" .. util.QuoteSafe( user.displayName ) .. "\" \"" .. questreward .. "\" " .. rewardcount ) end)
                     )
 end
 
@@ -93,7 +93,7 @@ function PLUGIN:cmdQuest( netuser, cmd, args )
     if (args[1] == "list") then
         rust.SendChatToUser( netuser, "Available Quests:" )
         for key,value in pairs(self.Quests) do
-            rust.SendChatToUser( netuser, rust.QuoteSafe(value.ID) .. ": " .. rust.QuoteSafe(value.Desc) )
+            rust.SendChatToUser( netuser, util.QuoteSafe(value.ID) .. ": " .. util.QuoteSafe(value.Desc) )
         end
         return
     end
@@ -104,13 +104,13 @@ function PLUGIN:cmdQuest( netuser, cmd, args )
             if ((value.Players[userID]) and (value.Players[userID].Value)) then
                 if (value.Objective(value.Players[userID].Value)) then
                     value.Reward(netuser)
-                    rust.SendChatToUser( netuser, "Completed quest " .. rust.QuoteSafe(value.ID) )
+                    rust.SendChatToUser( netuser, "Completed quest " .. util.QuoteSafe(value.ID) )
                     value.Players[userID] = nil
                 else
-                    rust.SendChatToUser( netuser, "Status of " .. rust.QuoteSafe(value.ID) .. ": " .. tostring(value.Players[userID].Value) )
+                    rust.SendChatToUser( netuser, "Status of " .. util.QuoteSafe(value.ID) .. ": " .. tostring(value.Players[userID].Value) )
                 end
             else
-                rust.SendChatToUser( netuser, "Status of " .. rust.QuoteSafe(value.ID) .. ": not joined")
+                rust.SendChatToUser( netuser, "Status of " .. util.QuoteSafe(value.ID) .. ": not joined")
             end
         end
         return
@@ -118,7 +118,7 @@ function PLUGIN:cmdQuest( netuser, cmd, args )
     
     if ((args[1] == "join") and (args[2])) then
         if (self:addPlayerToQuest(netuser, args[2])) then
-            rust.SendChatToUser( netuser, "Joined quest " .. rust.QuoteSafe(args[2]) )
+            rust.SendChatToUser( netuser, "Joined quest " .. util.QuoteSafe(args[2]) )
             return
         else
             rust.Notice( netuser, "/quest join error!" )
@@ -130,26 +130,29 @@ function PLUGIN:cmdQuest( netuser, cmd, args )
 
 end
 
-function PLUGIN:OnZombieKilled (zombiecontroller, dmg)
-    local player = dmg.attacker.client.netUser
-    local userID = rust.GetUserID( player )
-    
-    for key,value in pairs(self.Zombies) do
-        local quest = self.Quests[value]
-        if (quest.Players[userID]) then
-            if (not(quest.Players[userID].Value)) then
-                quest.Players[userID].Value = 0
-            end
-            quest.Players[userID].Value = quest.Players[userID].Value + 1
-            if (quest.Objective(quest.Players[userID].Value)) then
-                rust.SendChatToUser( player, "Completed quest " .. rust.QuoteSafe(quest.ID) )
-                quest.Reward(player)
-                quest.Players[userID] = nil
-            else
-                rust.SendChatToUser( player, "Status of " .. rust.QuoteSafe(quest.ID) .. ": " .. tostring(quest.Players[userID].Value) )
-            end
-        end
-    end
+function PLUGIN:OnKilled ( takedamage, event )
+		if (takedamage:GetComponent( "ZombieController" )) then
+
+				local player = dmg.attacker.client.netUser
+				local userID = rust.GetUserID(player)
+
+				for key,value in pairs(self.Zombies) do
+						local quest = self.Quests[value]
+						if (quest.Players[userID]) then
+								if (not(quest.Players[userID].Value)) then
+										quest.Players[userID].Value = 0
+								end
+								quest.Players[userID].Value = quest.Players[userID].Value + 1
+								if (quest.Objective(quest.Players[userID].Value)) then
+										rust.SendChatToUser( player, "Completed quest " .. util.QuoteSafe(quest.ID) )
+										quest.Reward(player)
+										quest.Players[userID] = nil
+								else
+										rust.SendChatToUser( player, "Status of " .. util.QuoteSafe(quest.ID) .. ": " .. tostring(quest.Players[userID].Value) )
+								end
+						end
+				end
+		end
 end
 
 function PLUGIN:SendHelpText( netuser )
